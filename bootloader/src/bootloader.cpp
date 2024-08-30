36910c6303e9bc1f2916610e99aa90a68dc4b2b1
Andreas Andersson <e.andreas.asson@gmail.com>
2024-08-30 17:35:35 +0200

    App sha256 verification (#3)
    * Add hash to app
    
    * Add black and flake8 to pre-commit
    
    * Merge python scripts and add output
    
    * Add read hash in bootloader
    
    * App is verified with sha256
    
    * Remove black python version
    
    * Set newer flake8 version

---
+++ bootloader/src/bootloader.cpp
@@ -0,0 +1,93 @@
+#include <mbedtls/sha256.h>
+
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+
+#include "RP2040.h"
+#include "common_definitions.h"
+#include "linker_definitions.h"
+#include "pico/stdlib.h"
+
+constexpr std::uint8_t SHA256_DIGEST_SIZE{32};
+
+static auto verify_hash(const uint32_t hash_address,
+                        const uint32_t app_size_address) -> bool {
+    printf("Verify app hash at %#X...\n", hash_address);
+
+    unsigned char stored_sha256[SHA256_DIGEST_SIZE];
+    memcpy(stored_sha256, (void *)hash_address, SHA256_DIGEST_SIZE);
+
+    mbedtls_sha256_context sha256_ctx;
+    mbedtls_sha256_init(&sha256_ctx);
+
+    int ret;
+    ret = mbedtls_sha256_starts_ret(&sha256_ctx, 0);
+    if (ret) {
+        return false;
+    }
+
+    auto image_start_address = ADDR_AS_U32(__APP_ADDRESS);
+    auto image_size = (size_t) * ((std::uint32_t *)app_size_address);
+    ret = mbedtls_sha256_update_ret(
+        &sha256_ctx, (const unsigned char *)image_start_address, image_size);
+    if (ret) {
+        return false;
+    }
+
+    unsigned char calculated_sha256[SHA256_DIGEST_SIZE];
+    ret = mbedtls_sha256_finish_ret(&sha256_ctx, calculated_sha256);
+    if (ret) {
+        return false;
+    }
+
+    mbedtls_sha256_free(&sha256_ctx);
+
+    // Compare calculated and stored hash
+    auto hash_matched{true};
+    for (int i = 0; i < SHA256_DIGEST_SIZE; i++) {
+        if (stored_sha256[i] != calculated_sha256[i]) {
+            hash_matched = false;
+        }
+    }
+    return hash_matched;
+}
+
+static void jump_to_vtor(const uint32_t vtor) {
+    typedef void (*funcPtr)();
+
+    printf("Start app at %#X...", vtor);
+
+    uint32_t reset_vector = *(volatile uint32_t *)(vtor + 0x04);
+    auto app_main = (funcPtr)reset_vector;
+
+    SCB->VTOR = (volatile uint32_t)(vtor);
+    app_main();
+}
+
+static void print_welcome_message() {
+    puts("");
+    puts("******************************************************");
+    puts("*                                                    *");
+    puts("*           Raspberry Pi Pico W Bootloader           *");
+    puts("*                                                    *");
+    puts("******************************************************");
+    puts("");
+}
+
+auto main() -> int {
+    stdio_uart_init_full(PICO_UART, PICO_UART_BAUD_RATE, PICO_UART_TX_PIN,
+                         PICO_UART_RX_PIN);
+    print_welcome_message();
+    sleep_ms(1000);
+
+    if (verify_hash(ADDR_AS_U32(__APP_HASH_ADDRESS),
+                    ADDR_AS_U32(__APP_SIZE_ADDRESS))) {
+        jump_to_vtor(ADDR_AS_U32(__APP_ADDRESS));
+    }
+    puts("Hash verification failed");
+    while (true) {
+    }
+
+    return 0;
+}
--- combine_bootloader_and_app.py
+++ combine_bootloader_and_app.py
@@ -1,3 +1,6 @@
+import hashlib
+
+BOOTLOADER_SIZE = 0x8000
 BOOTLOADER_FILE = "build/bootloader/PICO_BOOTLOADER.bin"
 APP_FILE = "build/example_app/PICO_BOOTLOADER_EXAMPLE_APP.bin"
 COMBINED_FILE = "build/PICO_BOOTLOADER_COMBINED.bin"
@@ -5,8 +8,36 @@ COMBINED_FILE = "build/PICO_BOOTLOADER_COMBINED.bin"
 with open(BOOTLOADER_FILE, "rb") as file:
     bootloader_raw_file = file.read()
 
+bootloader_padding = bytes(
+    [0xFF for _ in range(BOOTLOADER_SIZE - len(bootloader_raw_file))]
+)
+
+print(f"Add {len(bootloader_raw_file)} bytes bootloader")
+print(f"Add {len(bootloader_padding)} bytes bootloader padding")
+
 with open(APP_FILE, "rb") as file:
     app_raw_file = file.read()
 
-with open(COMBINED_FILE, 'wb') as file:
-    file.write(bootloader_raw_file + app_raw_file)
+app_hash = hashlib.sha256(app_raw_file).digest()
+app_size = len(app_raw_file).to_bytes(4, "little")
+app_info = app_hash + app_size
+app_info_padding = bytes([0xFF for _ in range(0x100 - len(app_info))])
+
+print(
+    f"Add {len(app_hash)} bytes app hash to binary: {hashlib.sha256(app_raw_file).hexdigest()}"
+)
+print(f"Add {len(app_size)} bytes app size")
+print(f"Add {len(app_info_padding)} bytes app info padding")
+print(f"Add {len(app_raw_file)} bytes app")
+
+with open(COMBINED_FILE, "wb") as file:
+    raw_content = (
+        bootloader_raw_file
+        + bootloader_padding
+        + app_info
+        + app_info_padding
+        + app_raw_file
+    )
+    file.write(raw_content)
+
+print(f"Total {len(raw_content)} bytes written")
