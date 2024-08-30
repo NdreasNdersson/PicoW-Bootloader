564ca0ec2c9114ed9295eb1e72346c45f583b3af
Andreas Andersson <e.andreas.asson@gmail.com>
2024-08-30 10:56:35 +0200

    Add black and flake8 to pre-commit

--- combine_bootloader_and_app.py
+++ combine_bootloader_and_app.py
@@ -1,5 +1,7 @@
 import hashlib
 
+# import cfiles
+
 BOOTLOADER_FILE = "build/bootloader/PICO_BOOTLOADER.bin"
 APP_FILE = "build/example_app/PICO_BOOTLOADER_EXAMPLE_APP.bin"
 COMBINED_FILE = "build/PICO_BOOTLOADER_COMBINED.bin"
@@ -13,5 +15,5 @@ with open(APP_FILE, "rb") as file:
 print(f"Add app hash to binary: {hashlib.sha256(app_raw_file).hexdigest()}")
 app_hash = hashlib.sha256(app_raw_file).digest()
 app_hash_padding = bytes([0xFF for _ in range(0x100 - len(app_hash))])
-with open(COMBINED_FILE, 'wb') as file:
+with open(COMBINED_FILE, "wb") as file:
     file.write(bootloader_raw_file + app_hash + app_hash_padding + app_raw_file)
--- pad_bootloader.py
+++ pad_bootloader.py
@@ -6,8 +6,11 @@ with open(BOOTLOADER_FILE, "rb") as file:
 
 bytes_to_pad = BOOTLOADER_SIZE - len(raw_file)
 if bytes_to_pad < 0:
-    raise ValueError(f"Bootloader '.bin' file can't be greater than {BOOTLOADER_SIZE}, but it's size is: {len(raw_file)}")
+    raise ValueError(
+        f"Bootloader '.bin' file can't be greater than {BOOTLOADER_SIZE}, \
+            but it's size is: {len(raw_file)}"
+    )
 
 padding = bytes([0xFF for _ in range(bytes_to_pad)])
-with open(BOOTLOADER_FILE, 'wb') as file:
+with open(BOOTLOADER_FILE, "wb") as file:
     file.write(raw_file + padding)
