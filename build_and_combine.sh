0b22e3802d52a5ed98296c039aa5dd66d35dea5a
Andreas Andersson <e.andreas.asson@gmail.com>
2024-08-30 11:51:45 +0200

    Merge python scripts and add output

--- build_and_combine.sh
+++ build_and_combine.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
+set -x
 
 cmake -B build -G Ninja
 cmake --build build
 
-python3 pad_bootloader.py
 python3 combine_bootloader_and_app.py
--- combine_bootloader_and_app.py
+++ combine_bootloader_and_app.py
@@ -1,7 +1,6 @@
 import hashlib
 
-# import cfiles
-
+BOOTLOADER_SIZE = 0x8000
 BOOTLOADER_FILE = "build/bootloader/PICO_BOOTLOADER.bin"
 APP_FILE = "build/example_app/PICO_BOOTLOADER_EXAMPLE_APP.bin"
 COMBINED_FILE = "build/PICO_BOOTLOADER_COMBINED.bin"
@@ -9,11 +8,34 @@ COMBINED_FILE = "build/PICO_BOOTLOADER_COMBINED.bin"
 with open(BOOTLOADER_FILE, "rb") as file:
     bootloader_raw_file = file.read()
 
+bootloader_padding = bytes(
+    [0xFF for _ in range(BOOTLOADER_SIZE - len(bootloader_raw_file))]
+)
+
+print(f"Add {len(bootloader_raw_file)} bytes bootloader")
+print(f"Add {len(bootloader_padding)} bytes bootloader padding")
+
 with open(APP_FILE, "rb") as file:
     app_raw_file = file.read()
 
-print(f"Add app hash to binary: {hashlib.sha256(app_raw_file).hexdigest()}")
+
 app_hash = hashlib.sha256(app_raw_file).digest()
-app_hash_padding = bytes([0xFF for _ in range(0x100 - len(app_hash))])
+app_info_padding = bytes([0xFF for _ in range(0x100 - len(app_hash))])
+
+print(
+    f"Add {len(app_hash)} bytes app hash to binary: {hashlib.sha256(app_raw_file).hexdigest()}"
+)
+print(f"Add {len(app_info_padding)} bytes app info padding")
+print(f"Add {len(app_raw_file)} bytes app")
+
 with open(COMBINED_FILE, "wb") as file:
-    file.write(bootloader_raw_file + app_hash + app_hash_padding + app_raw_file)
+    raw_content = (
+        bootloader_raw_file
+        + bootloader_padding
+        + app_hash
+        + app_info_padding
+        + app_raw_file
+    )
+    file.write(raw_content)
+
+print(f"Total {len(raw_content)} bytes written")
