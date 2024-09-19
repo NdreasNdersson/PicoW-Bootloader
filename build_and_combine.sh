26cb675b9699196b4bf6d1c65f796fe862980469
Andreas Andersson <e.andreas.asson@gmail.com>
2024-09-19 14:51:16 +0200

    Setup unittests

--- build_and_combine.sh
+++ build_and_combine.sh
@@ -1,7 +1,28 @@
 #! /bin/bash
 set -eux
 
-cmake -B build -G Ninja
-cmake --build build
+if [ $# -eq 0 ]; then
+    debug=0
+    cmake --preset ninja-release
+    cmake --build --preset ninja-release
+else
+    if [ "${1^^}" = "DEBUG" ]; then
+        debug=1
+    else
+        debug=0
+    fi
+fi
 
-./combine_bootloader_and_app.py
+if [ $debug = 1 ]; then
+    cmake --preset ninja-debug
+    cmake --build --preset ninja-debug
+    ./combine_bootloader_and_app.py \
+    --bootloader-file "build/ninja-debug/bootloader/PICO_BOOTLOADER.bin" \
+    --app-file "build/ninja-debug/example_app/PICO_BOOTLOADER_EXAMPLE_APP.bin"
+else
+    cmake --preset ninja-release
+    cmake --build --preset ninja-release
+    ./combine_bootloader_and_app.py \
+    --bootloader-file "build/ninja-release/bootloader/PICO_BOOTLOADER.bin" \
+    --app-file "build/ninja-release/example_app/PICO_BOOTLOADER_EXAMPLE_APP.bin"
+fi
